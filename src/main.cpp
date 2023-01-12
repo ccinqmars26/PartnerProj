06b0b3d99c5efcae4eb647aef929e6ba1419f457
ccinqmars26 <121891825+ccinqmars26@users.noreply.github.com>
2023-01-12 10:01:47 -0800

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
-#include <mbed.h>
+#inlcude <mbed.h>
+#include "HostIO.h"
+#include "SegDisplay.h"
 
 int main() {
-
-  // put your setup code here, to run once:
-
+  char data1, data2; 
+  SegInit; // initialize the 7-seg display 
+  HostInit(); initialize communiction with host computer
   while(1) {
-    // put your main code here, to run repeatedly:
-  }
+    data2 = GetKeyInput(); 
+    Seg2 = SegConvert(data2); 
+    data1 = GetKeyInput(); 
+    Seg1 = SegConvert(data1); 
+    printf(" "); // display spaces on host after recieved two digits
+      }
 }
